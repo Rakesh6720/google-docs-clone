d397fda890cbfc45e5a351268b554b5da2e2d458
Rakesh Baruah <rbaruah@crisisprevention.com>
2024-11-19 09:28:23 -0600

    Add Firebase configuration and dependency

---
+++ src/firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNryKj53abrpCdJOQrBpMIFNvCEZObtts",
+  authDomain: "docs-clone-c6494.firebaseapp.com",
+  projectId: "docs-clone-c6494",
+  storageBucket: "docs-clone-c6494.firebasestorage.app",
+  messagingSenderId: "332941327509",
+  appId: "1:332941327509:web:f0681cc1039da741e4af9c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
