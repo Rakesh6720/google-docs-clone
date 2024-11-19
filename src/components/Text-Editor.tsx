942072add56672af38af4c85c3d912368a228b30
Rakesh Baruah <rbaruah@crisisprevention.com>
2024-11-19 15:59:54 -0600

    Enhance TextEditor component to load initial content from Firestore and save changes in real-time; add local change tracking

--- src/components/Text-Editor.tsx
+++ src/components/Text-Editor.tsx
@@ -1,16 +1,21 @@
 import { useEffect, useRef, useState } from "react";
 import ReactQuill from "react-quill";
-import { setDoc, doc } from "firebase/firestore";
+import { setDoc, doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase-config";
+import "react-quill/dist/quill.snow.css";
+import "../App.css";
 
 export default function TextEditor() {
   const [IsEditing, SetIsEditing] = useState(false);
+
+  const isLocalChange = useRef(false);
+
   const quillRef = useRef<any>(null);
 
   const documentRef = doc(db, "documents", "sample-doc");
 
   const saveContent = () => {
-    if (quillRef.current) {
+    if (quillRef.current && isLocalChange.current) {
       const editor = quillRef.current.getEditor();
       const content = editor.getContents();
       console.log("Saving content to db: ", content);
@@ -18,21 +23,39 @@ export default function TextEditor() {
       setDoc(documentRef, { content: content.ops }, { merge: true })
         .then(() => console.log("Content saved"))
         .catch((error) => console.error("Error saving content: ", error));
+
+      isLocalChange.current = false;
     }
   };
 
   useEffect(() => {
     if (quillRef.current) {
       // load initial content from firestore db
+      getDoc(documentRef)
+        .then((doc) => {
+          if (doc.exists()) {
+            const data = doc.data();
+            if (data) {
+              const editor = quillRef.current.getEditor();
+              editor.setContents(data.content);
+            }
+          } else {
+            console.log("No doc found, starting with any empty editor");
+          }
+        })
+        .catch((error) => console.error("Error getting document:", error));
       // listen to firestore for any updates and update locally in real-time
       // listen for local text changes and save it to firestore
 
       const editor = quillRef.current.getEditor();
-      editor.on("text-change", () => {
-        SetIsEditing(true);
-        saveContent();
+      editor.on("text-change", (delta: any, oldDelta: any, source: any) => {
+        if (source === "user") {
+          isLocalChange.current = true;
+          SetIsEditing(true);
+          saveContent();
 
-        setTimeout(() => SetIsEditing(false), 5000);
+          setTimeout(() => SetIsEditing(false), 5000);
+        }
       });
     }
   }, []);
