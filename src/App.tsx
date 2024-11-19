1fdf898507101e4913700ee0cca6f8c42a044ad8
Rakesh Baruah <rbaruah@crisisprevention.com>
2024-11-19 15:24:33 -0600

    Remove dotenv import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import "./App.css";
 import { auth } from "./firebase-config";
-import "dotenv/config";
 import { signInAnonymously, onAuthStateChanged } from "firebase/auth";
 
 function App() {
